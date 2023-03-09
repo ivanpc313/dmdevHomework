c531f5cf5605e6f778632a0ebada9ccfb4f5e3a2
ivanpc313 <glybovskij313@mail.ru>
2023-03-09 14:43:50 +0300

    homework (arrays)

--- src/main/java/com/glybovskij/array/Task1.java
+++ src/main/java/com/glybovskij/array/Task1.java
@@ -1,2 +1,48 @@
-package com.glybovskij.array;public class Task1 {
+package com.glybovskij.array;
+
+/**
+ * Задание 1
+ * Дан одномерный массив целых чисел.
+ * Написать функцию, удаляющую из него все отрицательные элементы (удалить - значит создать новый массив с только положительными элементами).
+ * После удаления - умножить каждый элемент массива на его длину.
+ * Например: [3, 5, -6, 3, 2, -9, 0, -123] -> [15, 25, 15, 10, 0]
+ * <p>
+ * Не забывать, что всю логику приложения нужно выносить в отдельные функции. main - только для тестирования написанного функционала.
+ */
+public class Task1 {
+
+    public static void main(String[] args) {
+        int[] array = {3, 5, -6, 3, 2, -9, 0, -123};
+
+        int[] positiveArray = getPositiveArray(array);
+        printResultArray (positiveArray);
+    }
+
+    private static void printResultArray(int[] positiveArray) {
+        for (int i = 0; i < positiveArray.length; i++) {
+            positiveArray[i] = positiveArray[i] * positiveArray.length;
+            System.out.print(positiveArray[i] + " ");
+        }
+    }
+
+    private static int positiveSize(int[] array) {
+        int size = 0;
+        for (int j : array) {
+            if (j >= 0) {
+                size++;
+            }
+        }
+        return size;
+    }
+
+    private static int[] getPositiveArray(int[] array) {
+        int[] positiveArray = new int[positiveSize(array)];
+        for (int i = 0, index1 = 0; i < array.length; i++) {
+            if (array[i] >= 0) {
+                positiveArray[index1] = array[i];
+                index1++;
+            }
+        }
+        return positiveArray;
+    }
 }
--- src/main/java/com/glybovskij/array/Task2.java
+++ src/main/java/com/glybovskij/array/Task2.java
@@ -1,2 +1,43 @@
-package com.glybovskij.array;public class Task2 {
+package com.glybovskij.array;
+
+/**
+ * Задание 2
+ * Дан одномерный массив символов.
+ * Преобразовать его в одномерный массив чисел, где число - это код символа (любой символ - это число в памяти компьютера).
+ * Например: ['a', '6', 'y', 'P', 'T', 'q', '9', '+'] -> [97, 54, 121, 80, 84, 113, 57, 43]
+ * <p>
+ * Далее определить среднее арифметическое всех элементов целочисленного массива и вывести на консоль только те элементы,
+ * которые больше этого среднего арифметического.
+ */
+public class Task2 {
+
+    public static void main(String[] args) {
+        char[] array = {'a', '6', 'y', 'P', 'T', 'q', '9', '+' };
+
+        int[] generatedArray = generateToNumbers(array);
+        printResult(generatedArray);
+    }
+
+    private static void printResult(int[] array) {
+        int sum = 0;
+        double average;
+        for (int j : array) {
+            sum += j;
+        }
+        average = sum / (double) array.length;
+        for (int j : array) {
+            if (j > average) {
+                System.out.print(j + " ");
+            }
+        }
+    }
+
+    private static int[] generateToNumbers(char[] array) {
+        int[] generatedArray = new int[array.length];
+        for (int i = 0, index1 = 0; i < array.length; i++) {
+            generatedArray[index1] = array[i];
+            index1++;
+        }
+        return generatedArray;
+    }
 }
